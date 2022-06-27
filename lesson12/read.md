e448061718b7b50cc4674efaeaff10f9f14a4520
octavianCr <octaviancreanga991@gmail.com>
2022-06-27 23:27:03 +0300

    added a form page, still unresponsive

---
+++ lesson12/read.md
@@ -0,0 +1,4 @@
+implemented a form into a webpage
+
+the submit btn is curently not working 
+the project is still in progress
---
+++ lesson12/script.js
@@ -0,0 +1,10 @@
+
+
+
+// let form = document.getElementById('form');
+
+
+// let processing = document.createElement('span');
+
+
+// form.parentNode.insertBefore(processing, form);
