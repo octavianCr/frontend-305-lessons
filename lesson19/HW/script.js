7878ccc3c11ddd2d72685d30e9112fce0c77fdcb
octavianCr <octaviancreanga991@gmail.com>
2022-07-18 21:08:20 +0300

    homework js

---
+++ lesson19/HW/script.js
@@ -0,0 +1,66 @@
+
+// ______________________task1
+
+
+// let namee = prompt ("enter name");
+
+// alert("hello" + " " + namee );
+
+// ___________________________task2
+
+// let age = document.getElementById("user-age");
+
+// let ageInput = prompt ("enter age");
+
+// age.innerText = 2022 - ageInput ;
+
+// ___________________________task3
+
+// let squareSide = prompt ("enter square side lenght");
+
+// alert(squareSide * 4)
+
+// ___________________________task4
+
+// const pi = 3.14159;
+
+// let radius = prompt("circle radius");
+
+// alert(pi * (radius*2));
+
+// ___________________________task5
+
+// let distanceCovered = prompt("distance?  (km)");
+
+// let timeBetween = prompt("Time?  (hrs)");
+
+// alert(distanceCovered / timeBetween + "km/h2000");
+
+// ___________________________task6
+
+// let amountUsd = prompt("amount USD?");
+// let divisionFactor = .98 ;
+
+// alert (
+//     amountUsd * divisionFactor + " " + "EUR"
+// )
+
+// let exchangeRate = 1 * divisionFactor;
+// console.log(exchangeRate)
+
+// ___________________________task7
+
+// let memory = prompt("memory amount GB")
+
+// let operation = Math.trunc((memory*1000)/820)
+
+// alert(operation + ", " + "820mb files")
+
+// ___________________________task8
+
+
+
+
+
+
+
