8689cc72e7a3be01aeea7996266ac4fa43739139
octavianCr <octaviancreanga991@gmail.com>
2022-08-15 00:48:11 +0300

    added footer

--- lesson37/assets/js/script.js
+++ lesson37/assets/js/script.js
@@ -2,3 +2,16 @@ let sendBtn = document.getElementById("send-btn").addEventListener("click", func
     event.preventDefault()
     console.log(3);
 })
+
+
+function initMap() {
+    const uluru = { lat: -25.344, lng: 131.031 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+    }
